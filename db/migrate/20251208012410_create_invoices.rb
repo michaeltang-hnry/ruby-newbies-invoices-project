cb996d074b5258fdc90b300cd4b23f8249855977
Michael Zhang Tang <michael.tang@hnry.io>
2025-12-09 09:53:31 +1300

    rubocop linting

--- db/migrate/20251208012410_create_invoices.rb
+++ db/migrate/20251208012410_create_invoices.rb
@@ -2,7 +2,7 @@ class CreateInvoices < ActiveRecord::Migration[8.1]
   def change
     create_table :invoices do |t|
       t.string :identifier, null: false
-      
+
       t.decimal :total, precision: 10, scale: 2, default: 0.0
 
       t.index :identifier, unique: true
