729d1a2c819ad310736111ed28786ec6e1f2db42
Michael Zhang Tang <michael.tang@hnry.io>
2025-12-10 11:49:07 +1300

    added invoice controller

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,6 @@ class Invoice < ApplicationRecord
     validates :identifier, uniqueness: true
     belongs_to :client
     has_many :invoice_items, dependent: :destroy
+
+    accepts_nest_attributes_for :invoice_items, allow_destroy: true
 end
