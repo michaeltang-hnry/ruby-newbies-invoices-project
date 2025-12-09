a73466f8ed1bf5c37286cc43d663c01c922c2a0b
Michael Zhang Tang <michael.tang@hnry.io>
2025-12-09 15:47:39 +1300

    add client to invoices migration

--- db/migrate/20251208213315_add_client_to_invoices.rb
+++ db/migrate/20251208213315_add_client_to_invoices.rb
@@ -1,5 +1,9 @@
 class AddClientToInvoices < ActiveRecord::Migration[8.1]
-  def change
+  def up
     add_reference :invoices, :client, null: false, foreign_key: true
+  end
+
+  def down
+    remove_reference :invoices, :client, foreign_key: true
   end
 end
