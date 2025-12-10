dd9778a2072afbdf30bf8905f845e13d955ed210
Michael Zhang Tang <michael.tang@hnry.io>
2025-12-10 15:57:52 +1300

    fixed typo for nested attributes and added before validation callback for generating an unique identifier for invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,17 @@
 class Invoice < ApplicationRecord
-    validates :identifier, uniqueness: true
+    validates :identifier, uniqueness: true, presence: true
+    before_validation :generate_identifier, on: :create
+
     belongs_to :client
     has_many :invoice_items, dependent: :destroy
 
-    accepts_nest_attributes_for :invoice_items, allow_destroy: true
+    accepts_nested_attributes_for :invoice_items, allow_destroy: true
+
+    private
+
+    def generate_identifier
+        return if identifier.present?
+
+        self.identifier = "INV-#{SecureRandom.urlsafe_base64(10)}"
+    end
 end
