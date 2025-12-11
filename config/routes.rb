4ec8715db3f965ab150d51a37e81395c8308f501
Michael Zhang Tang <michael.tang@hnry.io>
2025-12-11 14:53:57 +1300

    added tailwind and basic invoice index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :invoices, only: [:index, :new, :create, :show], param: :identifier
+  root "invoices#index"
+
+  resources :invoices, only: [ :index, :new, :create, :show ], param: :identifier
 end
